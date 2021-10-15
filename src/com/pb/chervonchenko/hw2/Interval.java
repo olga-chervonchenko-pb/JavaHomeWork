9991a0937c19d025dcc1fc963d250a084ab84bca
Olga Chervonchenko <olga.chervonchenko@privatbank.ua>
2021-10-15 18:42:18 +0300

    init commit

---
+++ src/com/pb/chervonchenko/hw2/Interval.java
@@ -0,0 +1,31 @@
+package com.pb.chervonchenko.hw2;
+
+import java.util.Scanner;
+
+public class Interval {
+
+        public static void main(String[] args) {
+            int x;
+
+            Scanner result = new Scanner(System.in);
+            System.out.print("Введите число: ");
+            x = result.nextInt();
+
+            if (x <=14) {
+                System.out.println("Число попадает в промежуток 0-14");
+            }
+            else if (x >14 && x <= 35) {
+                System.out.println("Число попадает в промежуток 15-35");
+            }
+            else if (x >35 && x <= 50) {
+                System.out.println("Число попадает в промежуток 35-50");
+            }
+            else if (x >50 && x <= 100) {
+                System.out.println("Число попадает в промежуток 50-100");
+            }
+
+            else {
+                System.out.println("Число не попадает в нужные промежутки");
+            }
+        }
+}
